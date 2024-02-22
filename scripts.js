df9dbf6941776d764dc27039fbf1e7befc97a998
jgreiff-ds <josh@greiff.org>
2024-02-22 14:37:53 -0500

    storing function

--- scripts.js
+++ scripts.js
@@ -3,12 +3,33 @@ const questInput = document.getElementById("quest-title"); // get the input fiel
 const questList = document.getElementById("quests-list"); // get the list element
 const form = document.querySelector(".quest-form"); // get the form
 const noQuests = document.getElementById("no-quests"); // get the element that will show if there are no quests
-
+let quests = []
 form.addEventListener("submit", (event) => {
     event.preventDefault(); // prevent the default form submission
     const questTitle = questInput.value; // get the value of the input field
     const newQuest = document.createElement("li"); // create a new list item
     newQuest.textContent = questTitle; // set the text content of the new list item
     questList.appendChild(newQuest); // append the new list item to the list
+    quests.push(questTitle)
+    localStorage.setItem("quests", quests); // store the quest in local storage
     noQuests.style.display = "none"; // hide the "no quests" message
+    questInput.value = ""; // clear the input field
 })
+
+function populateQuests(){
+
+    const quest = localStorage.getItem("quests");
+    const questsArray = quest.split(",");
+    for(let i=0; i<questsArray.length; i++){
+        const newQuest = document.createElement("li");
+        newQuest.textContent = questsArray[i];
+        questList.appendChild(newQuest);
+    }
+
+    if(questsArray.length >= 0){
+        noQuests.style.display = "none";
+    }
+    
+}
+
+populateQuests();
